ac2ae33c7b259e590c5bd9dbfa2aee7d0769485e
itzkennedydev <kennedy@itskennedy.dev>
2023-08-06 00:43:01 -0500

    Search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/Sidebar";
 
 const font = Figtree({ subsets: ["latin"] });
 
